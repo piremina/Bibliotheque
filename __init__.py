1a4d86e3146d22b8b8f9e9f15e9ccd90628d2b1a
piremina <premi94240@gmail.com>
2025-01-22 15:48:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # Rend le fichier HTML dans le dossier templates
 
+
+@app.route('/enregistrement_livre', methods=['GET'])
+def formulaire_livre():
+    return render_template('enregistrement_livre.html')  # afficher le formulaire
+
+@app.route('/enregistrement_livre', methods=['POST'])
+def enregistrer_livre():
+    titre = request.form['titre']
+    auteur = request.form['auteur']
+
+    # Connexion à la base de données
+    conn = sqlite2.connect('database.db')
+    cursor = conn.cursor()
+
+    # Exécution de la requête SQL pour insérer un nouveau client
+    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (1002938, nom, prenom, "ICI"))
+    conn.commit()
+    conn.close()
+    return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
+
+
 # Démarrer l'application Flask
 if __name__ == "__main__":
     app.run(debug=True)
