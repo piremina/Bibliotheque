5ef703cd6e97eac3345ad80a41d5e4b7fdd83fc0
piremina <premi94240@gmail.com>
2025-01-22 15:10:55 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from datetime import datetime
-from urllib.request import urlopen
-import sqlite2
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+from flask import Flask, render_template
+
+# Initialiser l'application Flask
+app = Flask(__name__)
+
+# Définir la route pour la page /welcome
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html')
-  
+    return render_template('welcome.html')  # Rend le fichier HTML dans le dossier templates
+
+# Démarrer l'application Flask
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
