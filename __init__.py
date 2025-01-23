1c2ed6476953dedf687c69a1e00a419b986fc289
piremina <premi94240@gmail.com>
2025-01-23 09:20:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ def enregistrer_livre():
 
 @app.route('/recherche_livre/')
 def ReadBDD():
-    conn = sqlite2.connect('database.db')
+    conn = sqlite2.connect('bibliotheque.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM livres;')
     data = cursor.fetchall()
